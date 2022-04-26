56220bd4e06984609c82398985d347149afb391e
DimoDimov99 <dimoxd69@gmail.com>
2022-04-26 22:52:44 +0300

    added a option to add boilerplate without args

--- create_file.py
+++ create_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-
-
 import os 
 import time
 
-
+MAIN_BOILEPLATE_WITHOUT_ARGS = "#include <stdio.h>\n\nint main()\n{\n    return 0; \n}"
+MAIN_BOILEPLATE_WITH_ARGS = "#include <stdio.h>\n\nint main(int argc, char *argv[])\n{\n    return 0; \n}"
 CURRENT_DIRECTORY = os.getcwd()
 
 
@@ -27,10 +26,18 @@ def create_file(creation_times: int=1, in_separete_directory=False) -> None:
     label = 1
     extension = ".c"
     while (label <= creation_times):
-        file = user_input + str(label) + extension 
-        with open(file, "a", encoding="utf8") as file:
-            file.write("#include <stdio.h>\n\nint main(int argc, char *argv[])\n{\n    return 0; \n}")
+        file = user_input + str(label) + extension
+        to_include_arguments = input("Should the main function have arguments? (Y/N) : ").lower()
+        if to_include_arguments == 'n':
+            with open(file, "w", encoding="utf8") as file:
+                file.write(MAIN_BOILEPLATE_WITHOUT_ARGS)
             label+=1
+        elif to_include_arguments == 'y':
+            with open(file, "w", encoding="utf8") as file:
+                file.write(MAIN_BOILEPLATE_WITH_ARGS)
+            label+=1
+        else:
+            print("Invalid input! Please choose between 'y' and 'n'")
 
 
 create_file()
