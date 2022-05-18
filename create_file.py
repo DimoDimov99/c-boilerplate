e658ef7cb873e50ad550aec162d6b1d65dc6f548
DimoDimov99 <dimoxd69@gmail.com>
2022-05-18 21:28:23 +0300

    added functionality for creating each file with different name.Added some error handling

--- create_file.py
+++ create_file.py
@@ -1,43 +1,81 @@
 #!/usr/bin/env python3
-import os 
-import time
 
-MAIN_BOILEPLATE_WITHOUT_ARGS = "#include <stdio.h>\n\nint main()\n{\n    return 0; \n}"
-MAIN_BOILEPLATE_WITH_ARGS = "#include <stdio.h>\n\nint main(int argc, char *argv[])\n{\n    return 0; \n}"
+import os
+
+CURRENT_DIRECTORY = os.getcwd()
+
+MAIN_BOILEPLATE_WITHOUT_ARGS = "#include <stdio.h>\
+        \n\nint main()\n\
+        {\n    return 0; \n}"
+
+MAIN_BOILEPLATE_WITH_ARGS = "#include <stdio.h>\
+        \n\n\
+        int main(int argc, char *argv[])\
+        \n{\n    return 0; \n}"
+
 CURRENT_DIRECTORY = os.getcwd()
 
 
-def create_file(creation_times: int=1, in_separete_directory=False) -> None:
+def create_file(
+        creation_times: int = 1,
+        in_separete_directory=False,
+        incremented_names=False) -> None:
     """
     function to create .c boilerplate file/s
-    creation_times -> how many files to be created. default = 1
-    in_separate_directory -> should the file/s to be created in new directory inside the currrent directory. default = False
+
+    Args:
+        creation_times: int -> how many times files to be created
+        Default value 1
+        in_separete_directory: bool -> created files to be in directory
+        inside the main directory
+        Default value False
+        incremented_names: bool -> give one file name and every following
+        file that is created will have the name {filename(counter+=1).c}
+        Default value False
     """
     if in_separete_directory:
         user_input_directory = input("Enter the name of directory: ")
         directory = f"{CURRENT_DIRECTORY}/{user_input_directory}"
         try:
             os.mkdir(directory)
-        except OSError as error:
-            print(error)
-            return -1
+        except FileExistsError:
+            # print(error)
+            user_input = input(
+                    "Directory exist, do you want to enter it?: (y/n) "
+                    )
+            if user_input == 'y':
+                os.chdir(directory)
+            else:
+                print("Quiting...!")
+                return -1
         os.chdir(directory)
-    user_input = input("Enter your file name: ")
-    label = 1
+    counter = 1
     extension = ".c"
-    while (label <= creation_times):
-        file = user_input + str(label) + extension
-        to_include_arguments = input("Should the main function have arguments? (Y/N) : ").lower()
-        if to_include_arguments == 'n':
-            with open(file, "w", encoding="utf8") as file:
-                file.write(MAIN_BOILEPLATE_WITHOUT_ARGS)
-            label+=1
-        elif to_include_arguments == 'y':
-            with open(file, "w", encoding="utf8") as file:
-                file.write(MAIN_BOILEPLATE_WITH_ARGS)
-            label+=1
-        else:
+    if incremented_names:
+        user_input = input("Enter file name: ")
+    while (counter <= creation_times):
+        if not incremented_names:
+            user_input = input("Enter file name: ")
+            file = user_input + extension
+        file = user_input + str(counter) + extension
+        to_include_arguments = input(
+                "Should the main function have arguments? (Y/N) : "
+                ).lower()
+        while to_include_arguments != 'y' and to_include_arguments != 'n':
             print("Invalid input! Please choose between 'y' and 'n'")
+            to_include_arguments = input(
+                "Should the main function have arguments? (Y/N) : "
+                ).lower()
+        if to_include_arguments == 'n':
+            with open(file, "w+", encoding="utf8") as file:
+                file.write(MAIN_BOILEPLATE_WITHOUT_ARGS)
+            counter += 1
+        elif to_include_arguments == 'y':
+            with open(file, "w+", encoding="utf8") as file:
+                file.write(MAIN_BOILEPLATE_WITH_ARGS)
+            counter += 1
 
 
-create_file()
+create_file(
+        creation_times=3, in_separete_directory=True, incremented_names=False
+)
